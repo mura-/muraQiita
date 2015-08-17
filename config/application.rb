d2c8e210da46fa6a4e854970fbc66e6185c797c8
vagrant <vagrant@localhost.localdomain>
2015-08-18 05:29:10 +0900

    edit settings

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Muraqiita
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_spaces false
+    end
+  end
+end
