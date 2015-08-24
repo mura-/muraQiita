b7e6cd7dd84bc839172a9d873628a13857868b93
k.murahama <k.murahama@lexues.co.jp>
2015-08-24 21:45:51 +0900

    Routing整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  resource :user do
-    resources :stocks
-  end
-  get 'login' => 'sessions#new', as: :login
-  resource :session, only: [ :create, :destroy ]
-  resources :tips do
-    resources :comments
-  end
-
-  get 'mine' => 'tips#mine', as: :mine
-
   root 'tops#index'
 
+  resource :user, except:[ :destory ] do
+    resources :stocks, only: [ :index, :create, :destory ]
+  end
+
+  resource :session, only: [ :create, :destroy ]
+  get 'logout' => 'sessions#destroy', :as => :logout
   get '/auth/:provider/callback', :to => 'sessions#callback'
   post '/auth/:provider/callback', :to => 'sessions#callback'
-  get '/logout' => 'sessions#destroy', :as => :logout
+
+  resources :tips do
+    resources :comments, only: [ :create ]
+  end
+  get 'mine' => 'tips#mine', as: :mine
+
 end
